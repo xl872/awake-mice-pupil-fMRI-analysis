7c62786aa56d70e818fa49e67f31c01ef14edb13
xl872 <153525728+xl872@users.noreply.github.com>
2024-08-05 10:05:03 -0400

    Update pupil_anal.m

--- pupil-MATLAB/pupil_anal.m
+++ pupil-MATLAB/pupil_anal.m
@@ -240,164 +240,12 @@ M.c=c;M.cs=cs;M.lag=lag;M.lags=lags;M.keytime=keytime;M.L=L;M.U=U;M.cP=U-L;M.R=R
 %
 AD=M;
 
-% 
-% plot(time,AD.tm,'b',time,WT.tm,'r',LineWidth=2);
-% legend('averaged pupil response of AD','averaged pupil response of WT',Location='southeast',Box='off');
-% xlabel('Time/s')
-% ylabel('Pupil size (a.u.)')
-
-
-%%
-%         tt=zscore(smooth(squeeze(trail(1,1,:)),0.1,'loess'));
-%         [PKS,LOCS]=findpeaks(smooth(tt));
-%         figure;plot(tt);hold on;scatter(LOCS,PKS);hold off;
-%         figure;plot(diff(tt))
-
-
-%% model
-figure;
-subplot(2,3,1)
-time=1:1200;time=time/30;
-
-% plot(time,AD.tm,'b',time,WT.tm,'r',LineWidth=2); 
-% legend('averaged pupil response of AD','averaged pupil response of WT',Location='southeast',Box='off');
-% ylim([0 1]);
-% xlabel('Time/s')
-% ylabel('Pupil size (a.u.)')
-
-time_all=time;
-ymean = AD.tm';
-ystd= AD.ste;
-plot(time_all,ymean,'-','linewidth',2,'Color',"b")
-hold on
-h=fill([time_all,time_all(end:-1:1)],[ymean-ystd,ymean(end:-1:1)+ystd(end:-1:1)],'b');
-set(h,'FaceColor',"b",'FaceAlpha',0.5,'EdgeColor','none');
-
-
-ymean = WT.tm';
-ystd= WT.ste;
-plot(time_all,ymean,'-','linewidth',2,'Color',"r")
-
-h=fill([time_all,time_all(end:-1:1)],[ymean-ystd,ymean(end:-1:1)+ystd(end:-1:1)],'r');
-axis tight
-ylim([0 1])
-ylabel('Pupil size (a.u.)')
-set(h,'FaceColor',"r",'FaceAlpha',0.5,'EdgeColor','none');
-
-hold off
-legend('AD','','WT',Location='southeast',Box='off',NumColumns=2)
-xlabel('Time/s')
-
-
-
-
-subplot(2,3,4)
-tt=WT.tm;
-keytime1=find(diff(tt)<0,1);
-keytime2=find(tt(keytime1:end)<=1.25*min(tt(1:600)),1)+keytime(i,j,1)-1;
-kt=find(zscore(tt(keytime2:end))>0.75*min(zscore(tt(keytime2:end))))+keytime2-1;
-        keytime3=kt(find(kt>(keytime2+6*fps),1));
-        kt=find(zscore(tt(keytime3:end))>0.75*max(zscore(tt(keytime3:end))))+keytime3-1;
-        keytime4=kt(find(kt>(keytime3+0.2*fps),1));
-ttt=1/fps:1/fps:1/fps*length(tt(keytime3:keytime4));%keytime(i,j,3)/fps:1/fps:40;%
- Ut=mean(tt(keytime4:keytime4+0.1*fps))%min(keytime(i,j,4)+10*fps,length(tt)))
-        Lt=mean(tt(keytime3-0.1*fps:keytime3))
-
-        ft = fittype(['b*(1-exp(-x/a))+',num2str(Lt)]);
-        f=fit(ttt',smooth(tt(keytime3:keytime4),0.2),ft);
-        plot(f,'m-',ttt',tt(keytime3:keytime4),'r.'); 
-        legend('averaged pupil dialation of WT mice','fitted pupil dialation',Location='southeast',Box='off');
-        ylabel('Pupil size (a.u.)')
-        xlabel('Time (s)')
-        axis tight
-%         text(12,0.5,['{\itR}=',num2str(f.a)])
-
-subplot(2,3,5)
-tt=AD.tm;
-keytime1=find(diff(tt)<0,1);
-keytime2=find(tt(keytime1:end)<=1.25*min(tt(1:600)),1)+keytime(i,j,1)-1;
-kt=find(zscore(tt(keytime2:end))>0.75*min(zscore(tt(keytime2:end))))+keytime2-1;
-        keytime3=kt(find(kt>(keytime2+6*fps),1));
-        kt=find(zscore(tt(keytime3:end))>0.75*max(zscore(tt(keytime3:end))))+keytime3-1;
-        keytime4=kt(find(kt>(keytime3+0.2*fps),1));
-ttt=1/fps:1/fps:1/fps*length(tt(keytime3:keytime4));%keytime(i,j,3)/fps:1/fps:40;%
- Ut=mean(tt(keytime4:keytime4+0.1*fps))%min(keytime(i,j,4)+10*fps,length(tt)))
-        Lt=mean(tt(keytime3-0.1*fps:keytime3))
-
-        ft = fittype(['b*(1-exp(-x/a))+',num2str(Lt)]);
-        f=fit(ttt',smooth(tt(keytime3:keytime4),0.2),ft);
-        plot(f,'m-',ttt',tt(keytime3:keytime4),'b.');
-        legend('averaged pupil dialation of AD mice','fitted pupil dialation',Location='southeast',Box='off');
-        ylabel('Pupil size (a.u.)')
-        xlabel('Time (s)')
-        axis tight
-%         text(9,0.5,['{\itR}=',num2str(f.a)])
-
- subplot(2,3,3)
- rng('default')  % For reproducibility
-    x1 = AD.cP(:);
-    x2 = WT.cP(:);
-    x1(x1<0)=[];
-    x2(x2<0)=[];
-    x = [x1; x2];
-    g1 = repmat({'AD'},length(x1),1);
-    g2 = repmat({'WT'},length(x2),1);
-    g = [g1; g2];
-   % boxplot(x,g)
-   CategoricalScatterplot(x,g,'MarkerSize',3)
-%    Y{:,1}=x1;
-%     Y{:,2}=x2;
-% violin(Y,'facecolor',[1 1 0;0 1 0;.3 .3 .3;0 0.3 0.1],'edgecolor','none','bw',0.1,'mc','k','medc','r-.')
-
-
-   [h,p]= ttest2(x1,x2)
-    title({'$P_d$'},'interpreter','latex')
-    ylabel({'$P_d \rm{ (a.u.)}$'},'interpreter','latex')
-
-    subplot(2,3,6)
- rng('default')  % For reproducibility
-    x1 = AD.R(:);
-    x2 = WT.R(:);
-    x1(x1>50)=[];
-    x2(x2>50)=[];
-    x = [x1; x2];
-    
-    g1 = repmat({'AD'},length(x1),1);
-    g2 = repmat({'WT'},length(x2),1);
-    g = [g1; g2];
-    %boxplot(x,g)
-    CategoricalScatterplot(x,g,'MarkerSize',3)
-    [h,p]= ttest2(x1,x2)
-       title({'$R$'},'interpreter','latex')
-    ylabel({'$R \rm{ (a.u.)}$'},'interpreter','latex')
-
-   subplot(2,3,2)
- rng('default')  % For reproducibility
-    x1 = AD.dA(:);
-    x2 = WT.dA(:);
-        x1(x1<0)=[];
-    x2(x2<0)=[];
-    x = [x1; x2];
-    g1 = repmat({'AD'},length(x1),1);
-    g2 = repmat({'WT'},length(x2),1);
-    g = [g1; g2];
-    CategoricalScatterplot(x,g,'MarkerSize',3)
-    %boxplot(x,g)
-    [h,p]= ttest2(x1,x2)
-    title({'$P_c$'},'interpreter','latex')
-    ylabel({'$P_c \rm{ (a.u.)}$'},'interpreter','latex')
-    %%
-    %% model
+ %% model
     fps=30;
-figure;
-subplot(2,3,1)
+figure('Position',[100 100 500 150]);
+subplot(1,2,1)
 time=1:1200;time=time/30;
 
-% plot(time,AD.tm,'b',time,WT.tm,'r',LineWidth=2); 
-% legend('averaged pupil response of AD','averaged pupil response of WT',Location='southeast',Box='off');
-% ylim([0 1]);
-% xlabel('Time/s')
-% ylabel('Pupil size (a.u.)')
 
 time_all=time;
 ymean = AD.tm';
@@ -425,8 +273,9 @@ xlabel('Time/s')
 
 
 
-subplot(2,3,2)
+subplot(1,2,2)
 tt=WT.tm;
+keytime=WT.keytime;
 keytime1=find(diff(tt)<0,1);
 keytime2=find(tt(keytime1:end)<=1.25*min(tt(1:600)),1)+mean(keytime(:,:,1),[1 2])-1;
 kt=find(zscore(tt(keytime2:end))>0.75*min(zscore(tt(keytime2:end))))+keytime2-1;
@@ -449,6 +298,7 @@ ttt=1/fps:1/fps:1/fps*length(tt(keytime3:keytime4));%keytime(i,j,3)/fps:1/fps:40
 
 hold on
 tt=AD.tm;
+keytime=AD.keytime;
 keytime1=find(diff(tt)<0,1);
 keytime2=find(tt(keytime1:end)<=1.25*min(tt(1:600)),1)+mean(keytime(:,:,1),[1 2])-1;
 kt=find(zscore(tt(keytime2:end))>0.75*min(zscore(tt(keytime2:end))))+keytime2-1;
@@ -467,209 +317,109 @@ ttt=1/fps:1/fps:1/fps*length(tt(keytime3:keytime4));%keytime(i,j,3)/fps:1/fps:40
       axis([])
 %         text(9,0.5,['{\itR}=',num2str(f.a)])
 
- subplot(2,3,5)
+     %% group analysis mean per mouse
+     
+figure('Position',[100 100 500 150]);
+ subplot(1,3,2)
+ per=75;
  rng('default')  % For reproducibility
-    x1 = AD.cP(:);
-    x2 = WT.cP(:);
-    x1(x1<0)=[];
-    x2(x2<0)=[];
-    x = [x1; x2];
-    g1 = repmat({'AD'},length(x1),1);
-    g2 = repmat({'WT'},length(x2),1);
+    x1 = AD.cP;
+    x2 = WT.cP;
+    x1(x1<0)=nan;x2(x2<0)=nan;
+    ii=0;x11=[];x22=[];
+    for i = unique(ADindex)
+        ii=ii+1;
+        x11(ii) = trimmean(x1(:,ADindex==i),per,"all");
+%         xt = harmmean(x1,1,"omitnan");
+%         x11(ii) = harmmean(xt(ADindex==i),"all","omitnan");
+    end
+    ii=0;
+    for i = unique(WTindex)
+        ii=ii+1;
+        x22(ii) = trimmean(x2(:,WTindex==i),per,"all");
+%         xt = harmmean(x2,1,"omitnan");
+%         x22(ii) = harmmean(xt(WTindex==i),"all","omitnan");
+    end
+    
+    x = [x11'; x22'];
+    g1 = repmat({'AD'},length(x11),1);
+    g2 = repmat({'WT'},length(x22),1);
     g = [g1; g2];
    % boxplot(x,g)
-   CategoricalScatterplot(x,g,'MarkerSize',3)
-%    Y{:,1}=x1;
-%     Y{:,2}=x2;
-% violin(Y,'facecolor',[1 1 0;0 1 0;.3 .3 .3;0 0.3 0.1],'edgecolor','none','bw',0.1,'mc','k','medc','r-.')
+   CategoricalScatterplot(x,g,'MarkerSize',8,'BoxLineWidth',1.5,...
+   'MedianLineWidth',1.5,'WhiskerLineWidth',1.5)
 
-
-   [h,p]= ttest2(x1,x2)
+   [h,p]= ttest2(x11,x22)
     title({'$P_d$'},'interpreter','latex')
     ylabel({'$P_d \rm{ (a.u.)}$'},'interpreter','latex')
+ylim([0.1 0.8])
 
-    subplot(2,3,6)
+    subplot(1,3,3)
  rng('default')  % For reproducibility
-    x1 = AD.R(:);
-    x2 = WT.R(:);
-    x1(x1>50)=[];
-    x2(x2>50)=[];
-    x = [x1; x2];
+    x1 = AD.R;
+    x2 = WT.R;
+    x1(x1>100)=nan;x2(x2>100)=nan;
+    ii=0;x11=[];x22=[];
+    for i = unique(ADindex)
+        ii=ii+1;
+        x11(ii) = trimmean(x1(:,ADindex==i),per,"all");
+%         xt = harmmean(x1,1,"omitnan");
+%         x11(ii) = harmmean(xt(ADindex==i),"all","omitnan");
+    end
+    ii=0;
+    for i = unique(WTindex)
+        ii=ii+1;
+        x22(ii) = trimmean(x2(:,WTindex==i),per,"all");
+%         xt = harmmean(x2,1,"omitnan");
+%         x22(ii) = harmmean(xt(WTindex==i),"all","omitnan");
+    end
     
-    g1 = repmat({'AD'},length(x1),1);
-    g2 = repmat({'WT'},length(x2),1);
+    x = [x11'; x22'];
+    g1 = repmat({'AD'},length(x11),1);
+    g2 = repmat({'WT'},length(x22),1);
+
     g = [g1; g2];
     %boxplot(x,g)
-    CategoricalScatterplot(x,g,'MarkerSize',3)
-    [h,p]= ttest2(x1,x2)
+    CategoricalScatterplot(x,g,'MarkerSize',8,'BoxLineWidth',1.5,...
+   'MedianLineWidth',1.5,'WhiskerLineWidth',1.5)
+    [h,p]= ttest2(x11,x22)
+    %ylim([5 30])
        title({'$T$'},'interpreter','latex')
     ylabel({'$T \rm{ (a.u.)}$'},'interpreter','latex')
 
-   subplot(2,3,4)
+   subplot(1,3,1)
  rng('default')  % For reproducibility
-    x1 = AD.dA(:);
-    x2 = WT.dA(:);
-        x1(x1<0)=[];
-    x2(x2<0)=[];
-    x = [x1; x2];
-    g1 = repmat({'AD'},length(x1),1);
-    g2 = repmat({'WT'},length(x2),1);
+    x1 = AD.dA;
+    x2 = WT.dA;
+    x1(x1<0)=nan;x2(x2<0)=nan;
+    ii=0;x11=[];x22=[];
+    for i = unique(ADindex)
+        ii=ii+1;
+        x11(ii) = trimmean(x1(:,ADindex==i),per,"all");
+%         xt = harmmean(x1,1,"omitnan");
+%         x11(ii) = harmmean(xt(ADindex==i),"all","omitnan");
+    end
+    ii=0;
+    for i = unique(WTindex)
+        ii=ii+1;
+        x22(ii) = trimmean(x2(:,WTindex==i),per,"all");
+%         xt = harmmean(x2,1,"omitnan");
+%         x22(ii) = harmmean(xt(WTindex==i),"all","omitnan");
+    end
+    
+    x = [x11'; x22'];
+    g1 = repmat({'AD'},length(x11),1);
+    g2 = repmat({'WT'},length(x22),1);
     g = [g1; g2];
-    CategoricalScatterplot(x,g,'MarkerSize',3)
+    CategoricalScatterplot(x,g,'MarkerSize',8,'BoxLineWidth',1.5,...
+   'MedianLineWidth',1.5,'WhiskerLineWidth',1.5)
+    ylim([0.1 1])
     %boxplot(x,g)
-    [h,p]= ttest2(x1,x2)
+    [h,p]= ttest2(x11,x22)
     title({'$P_c$'},'interpreter','latex')
     ylabel({'$P_c \rm{ (a.u.)}$'},'interpreter','latex')
     
-%% box
-% figure;
-rng('default')  % For reproducibility
-x1 = AD.scm(:);
-x2 = WT.scm(:);
-x = [x1; x2];
-g1 = repmat({'AD'},length(x1),1);
-g2 = repmat({'WT'},length(x2),1);
-g = [g1; g2];
-boxplot(x,g)
-[h,p]= ttest2(x1,x2)
-
-% title('Compare Maximum Cross-correlation from Different Group of Mouse')
-%% box
-% figure;
-rng('default')  % For reproducibility
-x3 = AD.D(:);
-x4 = WT.D(:);
-x = [x3;x4];
-% g1 = repmat({'AD'},length(x1),1);
-% g2 = repmat({'WT'},length(x2),1);
-
-g3 = repmat({'AD'},length(x3),1);
-g4 = repmat({'WT'},length(x4),1);
-g = [g3;g4];
-boxplot(x,g)
-ylabel('Dialation duration [s]')
-[h,p]= ttest2(x3,x4)
-% title('Compare Maximum Cross-correlation from Different Group of Mouse')
-%% box
-% figure;
-rng('default')  % For reproducibility
-x3 = AD.keytime(:,:,4)/30-AD.keytime(:,:,2)/30;
-x4 = WT.keytime(:,:,4)/30-WT.keytime(:,:,2)/30;
-x3=x3(:);x4=x4(:);
-x = [x3;x4];
-% g1 = repmat({'AD'},length(x1),1);
-% g2 = repmat({'WT'},length(x2),1);
-
-g3 = repmat({'AD_T4-T2'},length(x3),1);
-g4 = repmat({'WT_T4-T2'},length(x4),1);
-g = [g3;g4];
-boxplot(x,g)
-[h,p]= ttest2(x3,x4)
-% title('Compare Maximum Cross-correlation from Different Group of Mouse')
-%% box
-% figure;
-rng('default')  % For reproducibility
-x3 = AD.keytime(:,:,1)/30;
-x4 = WT.keytime(:,:,1)/30;
-x3=x3(:);x4=x4(:);
-x = [x3;x4];
-% g1 = repmat({'AD'},length(x1),1);
-% g2 = repmat({'WT'},length(x2),1);
-
-g3 = repmat({'AD_T1'},length(x3),1);
-g4 = repmat({'WT_T1'},length(x4),1);
-g = [g3;g4];
-boxplot(x,g)
-[h,p]= ttest2(x3,x4)
-% title('Compare Maximum Cross-correlation from Different Group of Mouse')
-%%
-rng('default')  % For reproducibility
-x3 = AD.c;
-x4 = WT.c;
-x3=x3(:);x4=x4(:);
-x = [x3;x4];
-% g1 = repmat({'AD'},length(x1),1);
-% g2 = repmat({'WT'},length(x2),1);
-
-g3 = repmat({'AD'},length(x3),1);
-g4 = repmat({'WT'},length(x4),1);
-g = [g3;g4];
-boxplot(x,g)
-[h,p]= ttest2(x3,x4)
-% title('Compare Maximum Cross-correlation from Different Group of Mouse')
-
-%%
-figure;
-
-time_all=1:12300;time_all=time_all/30;
-ymean = mean(AD.trail,1);
-ystd= std(AD.trail,1);
-plot(time_all,ymean,'-','linewidth',2,'Color',"b")
-hold on
-h=fill([time_all,time_all(end:-1:1)],[ymean-ystd,ymean(end:-1:1)+ystd(end:-1:1)],'b');
-set(h,'FaceColor',"b",'FaceAlpha',0.5,'EdgeColor','none');
-
-
-time_all=1:12300;time_all=time_all/30;
-ymean = mean(WT.trail,1);
-ystd= std(WT.trail,1);
-plot(time_all,ymean,'-','linewidth',2,'Color',"r")
-
-h=fill([time_all,time_all(end:-1:1)],[ymean-ystd,ymean(end:-1:1)+ystd(end:-1:1)],'r');
-axis tight
-ylim([0 1])
-ylabel('Pupil size (a.u.)')
-set(h,'FaceColor',"r",'FaceAlpha',0.5,'EdgeColor','none');
-for i=0:9
-    hold on
-    tstim=40*i+10:40*i+10+8;
-plot(tstim,zeros(length(tstim),1),"LineWidth",2,"Color",'c');
-end
-
-hold off
-legend('AD','','WT',Location='southeast',Box='off',NumColumns=2)
-xlabel('Time/s')
-
-%%
-figure;
-time_all=1:10;
-ymean = mean(AD.c,2)';
-ystd= std(AD.c',1);
-plot(time_all,ymean,'-','linewidth',2,'Color',"#0072BD")
-hold on
-h=fill([time_all,time_all(end:-1:1)],[ymean-ystd,ymean(end:-1:1)+ystd(end:-1:1)],'b');
-set(h,'FaceColor',"#0072BD",'FaceAlpha',0.5,'EdgeColor','none');
-hold on
-
-time_all=1:10;
-ymean = mean(WT.c,2)';
-ystd= std(WT.c',1);
-plot(time_all,ymean,'-','linewidth',2,'Color',"#D95319")
-hold on
-h=fill([time_all,time_all(end:-1:1)],[ymean-ystd,ymean(end:-1:1)+ystd(end:-1:1)],'r');
-set(h,'FaceColor',"#D95319",'FaceAlpha',0.5,'EdgeColor','none');
-
-
-
-hold off
-legend('AD','','WT',Location='southeast',Box='off')
-xlabel('stimulations in a trail')
-
-
-%% wt only 
-figure;
-
-time=1:1200;time=time/30;
-
-time_all=time;
-
-
-ymean = WT.tm';
-ystd= WT.ste;
-plot(time_all,ymean,'-','linewidth',2,'Color',"k")
-
-xlabel('Time/s')
-fontsize(gcf,14,"points")
+ 
 
 
 
